834e8abb906095f16debb50a2c47134bfae8b588
unknown <Sascha@DESKTOP-4T2QKL1.wbr.lan>
2020-12-08 02:31:57 +0100

    final changes

--- etc/srvcheck/restic_backup_local.sh
+++ etc/srvcheck/restic_backup_local.sh
@@ -7,18 +7,18 @@ set -e -o pipefail
 
 # key and value pairs of service url and hc-ping.com uuid
 declare -A SERVICES
-SERVICES["b565600b-02b1-4a1f-9455-23dc918d2022"]="http://grey.dmz.local/portainer/"
+
+# Add all your services here, you need to create a hc-ping entry first
+SERVICES["add your hc-ping id here!"]="<service-url>"
 
 # if the url is reachable, then it returns a error code of 0 (OK)
 checkService() {
 	HTTP_RESPONSE=$(curl -fsS --retry 5 --write-out %{http_code} --output /dev/null $1)
 
 	if [ "$HTTP_RESPONSE" == "200" ]; then
-		echo "checkService OK" $1
 		# 0 = true
 		return 0
 	else
-		echo "checkService KO" $1
 		# 1 = false
 		return 1
 	fi
@@ -29,16 +29,17 @@ startPing() {
 	curl -fsS -m 10 --retry 5 "https://hc-ping.com/${1}/start"
 }
 
+# call it without an 2nd argument to return a successful error code
 sendPing() {
 	# $1 should always be the uuid of the service
 	# $2 should be the error code
 	if [ -z "$2" ]; then
-		echo "has none"
 		curl -fsS -m 10 --retry 5 "https://hc-ping.com/${1}"
+		# 0 = true
 		return 0
 	else
-		echo "has $2"
 		curl -fsS -m 10 --retry 5 "https://hc-ping.com/${1}/${2}"
+		# 0 = true
 		return 0
 	fi
 }
@@ -47,9 +48,13 @@ sendPing() {
 for UUID in ${!SERVICES[@]}; do
 	startPing "${UUID}"
 	if checkService "${SERVICES[${UUID}]}"; then
+		# 0 = No Error
 		sendPing "${UUID}" "0"
 	else
+		# 1 = Error
 		sendPing "${UUID}" "1"
 	fi
 	echo ${UUID} ${SERVICES[${UUID}]}
+	# just to give hc-ping a rest and not to trigger the rate limiter
+	sleep 1
 done
